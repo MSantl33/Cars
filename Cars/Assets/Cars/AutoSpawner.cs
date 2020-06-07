021d89b4db7f491bc12e4c4f441344f88ca76d4d
Matejas S <matejas.santl@student.mev.hr>
2020-06-08 00:35:43 +0200

    s

---
+++ Cars/Assets/Cars/AutoSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSpawner : MonoBehaviour {
+
+	public GameObject[] Cars;
+	int CarNo;
+	public float maxPos=2.45f;
+	public float delayTimer = 1f;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = delayTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			Vector3 carPos = new Vector3 (Random.Range (2.45f, -2.45f), transform.position.y, transform.position.z);
+			CarNo = Random.Range (0, 5);
+			Instantiate (Cars [CarNo], carPos, transform.rotation);
+			timer = delayTimer;
+		}
+	}
+}
---
+++ Cars/Assets/Cars/KontrolaAuta.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KontrolaAuta : MonoBehaviour {
+
+	public float carSpeed;
+	public float MaxPos=2.3f;
+	Vector3 position;
+	public Rezultat UI;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		position.x += Input.GetAxis ("Horizontal") * carSpeed * Time.deltaTime;
+		position.x=Mathf.Clamp(position.x,-2.4f,2.4f);
+		transform.position = position;
+
+		position.y += Input.GetAxis ("Vertical") * carSpeed * Time.deltaTime;
+		position.x=Mathf.Clamp(position.x,-4f,3.2f);
+		transform.position = position;
+
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "ProtivnickiAuto") {
+			Destroy (gameObject);
+			UI.GameOverActivated ();
+		}
+	}
+}
